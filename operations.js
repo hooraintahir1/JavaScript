d16afb1370b7e2acf92bf134d025b1256762ab05
Hoorain <hooraintahir5@gmail.com>
2026-02-05 23:52:49 +0500

    learnt different operations

---
+++ operations.js
@@ -0,0 +1,13 @@
+let num = 3
+let negNum = -num
+
+console.log(negNum); 
+
+// Power operator
+console.log(2**2); // Gives power of number(2)
+
+let str1 = "Hi"
+let str2 = "Hoorain"
+let str3 = str1 + " " + str2
+console.log(str3); // Concatenation of strings
+

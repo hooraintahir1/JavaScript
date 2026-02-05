ec4c44e13879ae299787cdd31ac9239eaf146803
Hoorain <hooraintahir5@gmail.com>
2026-02-06 02:27:36 +0500

    new

---
+++ strings.js
@@ -0,0 +1,17 @@
+let name = "hoorain"
+let age = 20
+
+console.log('my name is ${name} and my age is ${age}');
+
+const gamename = new String("Clash of clans")
+console.log(gamename[4]);
+console.log(gamename.toUpperCase());
+
+
+const newstring = "   Hello   "
+console.log(newstring.trim());
+
+const url = "https://www.hoorain.com/hoorain%20tahir"
+console.log(url.replace("%20", "-"));
+
+

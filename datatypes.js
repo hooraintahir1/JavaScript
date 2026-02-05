6d27d8a2a9a5156b2f678244920e92048416b1f0
Hoorain <hooraintahir5@gmail.com>
2026-02-06 01:36:08 +0500

    new datatypes

---
+++ datatypes.js
@@ -0,0 +1,13 @@
+console.log(2<1)
+console.log(2==1)
+console.log(2!=1)
+
+console.log(null == 0)
+console.log(null >= 0)
+console.log(null < 0) // comparison of null with equality operator converts null to 0 but just the 
+// comparison of null with no equality operator does not convert null to 0 and gives false.
+
+//=== is a strict checker
+
+//arrays,objects,functions are non-primitive data types
+

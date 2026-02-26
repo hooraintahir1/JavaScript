a8afb457e49ede199c16e37be4ed974f5ac4b957
Hoorain <hooraintahir5@gmail.com>
2026-02-27 00:57:27 +0500

    date & time

---
+++ date.js
@@ -0,0 +1,37 @@
+let mydate = new Date()
+console.log(mydate.toString());
+console.log(mydate.toDateString());
+console.log(mydate.toLocaleString());
+console.log(typeof mydate);
+
+let mystamp = Date.now()
+console.log(Date.now()/1000); // converted to seconds 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
